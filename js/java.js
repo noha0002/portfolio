c1179e366e3f3854874b6b5dd0bded1dcd070510
Noraa <noha0002@stud.kea.dk>
2025-06-10 21:26:51 +0200

    ændringer

--- js/java.js
+++ js/java.js
@@ -2,6 +2,7 @@ document.querySelectorAll(".hover1, .hover2, .hover3, .hover4")
   .forEach((item) => {
     const pBox = item.querySelector(".p-box");
     if (pBox) pBox.style.visibility = "hidden";
+    
 
     item.addEventListener("mouseenter", () => {
       if (pBox) pBox.style.visibility = "visible";
@@ -15,6 +16,11 @@ document.querySelectorAll(".hover1, .hover2, .hover3, .hover4")
     });
   });
 
+
+
+  
+
+
  
   
 
@@ -26,6 +32,4 @@ document.querySelectorAll(".hover1, .hover2, .hover3, .hover4")
 
   
 
-//el betyder "element" så det du gør, er "selecting ur .b-box"
-// og hver p-box "foreach" for elementet "style.backgroundcolor" "
-// pBox.style.visibility = "hidden"; " den skal først gemmes før, det vises.
+
